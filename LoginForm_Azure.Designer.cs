21f8ddd98805928b3c18b260978227a0346947ed
Bruno Pescarolli <bruno.pescarolli@hotmail.com>
2023-02-01 14:26:11 -0300

    init

---
+++ LoginForm_Azure.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace MSAL_dotnet6
+{
+    partial class LoginForm_Azure
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.logginBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(29, 53);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 50);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "MSAL dotnet 6";
+            // 
+            // logginBtn
+            // 
+            this.logginBtn.Location = new System.Drawing.Point(63, 130);
+            this.logginBtn.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.logginBtn.Name = "logginBtn";
+            this.logginBtn.Size = new System.Drawing.Size(198, 44);
+            this.logginBtn.TabIndex = 2;
+            this.logginBtn.Text = "Sign in with Microsoft";
+            this.logginBtn.UseVisualStyleBackColor = true;
+            this.logginBtn.Click += new System.EventHandler(this.logginBtn_Click);
+            // 
+            // LoginForm_Azure
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(344, 227);
+            this.Controls.Add(this.logginBtn);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "LoginForm_Azure";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MSAL test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button logginBtn;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.Identity.Client;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace MSAL_dotnet6
+{
+    internal static class Program
+    {
+        public static Production appInfo = Newtonsoft.Json.JsonConvert.DeserializeObject<Production>(Encoding.UTF8.GetString(MSAL_dotnet6.Resources.appinfo));
+        public static string ClientId = appInfo.ClientID;
+        public static string Tenant = appInfo.TenantID;
+        private static IPublicClientApplication clientApp;
+
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            InitializeAuth();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            //ApplicationConfiguration.Initialize();
+            Application.Run(new LoginForm_Azure());
+        }
+
+        public static IPublicClientApplication PublicClientApp { get { return clientApp; } }
+
+        private static void InitializeAuth()
+        {
+
+            clientApp = PublicClientApplicationBuilder.Create(ClientId)
+                   .WithRedirectUri("https://login.microsoftonline.com/common/oauth2/nativeclient")
+                   //.WithDefaultRedirectUri()
+                   .WithAuthority(AzureCloudInstance.AzurePublic, Tenant)
+                   .Build();
+            //TokenCacheHelper.EnableSerialization(clientApp.UserTokenCache);
+        }
+
+    }
+}
